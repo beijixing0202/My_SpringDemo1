72bbad84a0053c04e6223b08b67ce0ba99eb90a0
beijixing0202 <wangsy0202@163.com>
2019-08-16 21:05:09 +0800

    提交Spring-mybatis整合Demo

--- src/main/java/net/bill99/base/BaseTest.java
+++ src/main/java/net/bill99/base/BaseTest.java
@@ -2,6 +2,18 @@ package net.bill99.base;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.DataProvider;
+
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
 @ContextConfiguration(locations = {"classpath:qacontext/applicationContext.xml"})
 public class BaseTest extends AbstractTestNGSpringContextTests {
+
+    //迭代器
+    @DataProvider(name="data")
+    public Iterator<Object[]> dataForTestMethod(Method method) throws FileNotFoundException {
+        return (Iterator<Object[]>) new ExcelDataProvider(this.getClass().getSimpleName(), method.getName());
+    }
 }
